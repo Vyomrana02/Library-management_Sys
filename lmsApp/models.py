6374b6174d6274b0619127d8f36871370879070a
Vyomrana02 <vyom.rana02@gmail.com>
2022-06-12 17:18:39 +0530

    image-partial

--- lmsApp/models.py
+++ lmsApp/models.py
@@ -5,8 +5,8 @@ from django.utils import timezone
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# from PIL import Image
-# from django.contrib.auth.models import User
+from PIL import Image
+from django.contrib.auth.models import User
 from django.contrib.auth.base_user import BaseUserManager
 
 
