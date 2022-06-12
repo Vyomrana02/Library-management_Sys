e83cda8afcc380504b8884295a1dafb4c52b6e24
Vyomrana02 <vyom.rana02@gmail.com>
2022-06-12 17:15:49 +0530

    app

--- lmsApp/apps.py
+++ lmsApp/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 
 class lmsAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    # default_auto_field = 'django.db.models.BigAutoField'
     name = 'lmsApp'
